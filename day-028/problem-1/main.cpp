90228cdb9f33a77f2565ea87710ccf11d4edaa4d
Nick Kane <nkanedev@gmail.com>
2017-04-19 18:09:49 -0400

    adding setup problem for day 28

---
+++ day-028/problem-1/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * Write a program that displays the following two prompts:
+ * "Enter a month (use 1 for Jan, etc):"
+ * "Enter a day of the month:"
+ * 
+ * Your program should accept and store a number in a variable
+ * in response to the first prompt, and accept and store a number
+ * in a variable named day in response to the second prompt. If
+ * the month entered is not between 1 and 12 inclusive, the program
+ * should print a message informing the user that an invalid month
+ * has been entered. If the day entered is not between 1 and 31 inclusive,
+ * the program should print a message informing the user that an invalid
+ * day has been entered.
+ *
+ * What will your program do if the user types a number with a decimal point
+ * for the month? How can you ensure that your if statements check for an
+ * integer number?
+ *
+ * In a non-leap year, February has 28 days; the months January, March, May
+ * July, August, October, and December has 31 days; and all the other months
+ * have 30 days. Using this information, the program needs to display a message
+ * when an invalid day is entered for a user-entered month - for this program
+ * ignore leap years.
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
