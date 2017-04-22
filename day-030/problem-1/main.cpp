77573c3dc56dd063b156d5db4504bf1b2053a53c
Nick Kane <nkanedev@gmail.com>
2017-04-22 11:46:21 -0400

    complete day 30 problem 1

--- day-030/problem-1/main.cpp
+++ day-030/problem-1/main.cpp
@@ -17,15 +17,141 @@
  * as follows:
  * 
  * rootOne = -b + sqrt(b^2 - 4ac) / 2a
- * rootTwo = -b - sqrt(b^2 = 4ac) / 2a
+ * rootTwo = -b - sqrt(b^2 - 4ac) / 2a
  * 
  * Using these equations, write a C program to solve for the roots of a quadratic equation.
  */
 
 #include <stdio.h>
+#include <math.h>
+
+struct QuadEquation
+{
+	float A;
+	float B;
+	float C;
+	float RootOne;
+	float IRootOne;
+	float RootTwo;
+	float IRootTwo;
+};
+
+inline QuadEquation
+CreateQuadEquation(float, float, float);
+
+inline float
+ComputeFirstRootQuadEquation(QuadEquation *);
+
+inline float
+ComputeSecondRootQuadEquation(QuadEquation *);
+
+inline void
+PrettyPrint(QuadEquation *);
 
 int
 main()
 {
+	QuadEquation E1 = CreateQuadEquation(5.0f, 6.0f, 2.0f);
+	QuadEquation E2 = CreateQuadEquation(1.0f, -7.0f, 20.0f);
+	QuadEquation E3 = CreateQuadEquation(34.0f, 0.0f, 16.0f);
+
+	PrettyPrint(&E1);
+	PrettyPrint(&E2);
+	PrettyPrint(&E3);
+
 	return 0;
+}
+
+inline QuadEquation
+CreateQuadEquation(float a, float b, float c)
+{
+	QuadEquation Q =
+	{
+		a,
+		b,
+		c,
+		0.0f,
+		0.0f,
+		0.0f,
+		0.0f,
+	};
+
+	Q.RootOne = ComputeFirstRootQuadEquation(&Q);
+	Q.RootTwo = ComputeSecondRootQuadEquation(&Q);
+
+	return Q;
+}
+
+inline float
+ComputeFirstRootQuadEquation(QuadEquation *q)
+{
+	float result = 0.0f;
+
+	result = pow(q->B, 2);
+	result = (result - (4.0f * q->A * q->C));
+	if (result > 0.0f)
+	{
+		result = sqrt(result);
+	}
+	else
+	{
+		// can't take a negative numbers square root!
+		q->IRootOne = sqrt((result * -1.0f));
+		q->IRootOne = (q->IRootOne / (2.0f * q->A));
+		result = 0.0f;
+	}
+
+	result = ((-1.0f * q->B) + result);
+	result = (result / (2.0f * q->A));
+	
+	return result;
+}
+
+inline float
+ComputeSecondRootQuadEquation(QuadEquation *q)
+{
+	float result = 0.0f;
+
+	result = pow(q->B, 2);
+	result = (result - (4.0f * q->A * q->C));
+
+	if (result > 0.0f)
+	{
+		result = sqrt(result);
+	}
+	else
+	{
+		// can't take a negative numbers square root!
+		q->IRootTwo = sqrt((result * -1.0f));
+		q->IRootTwo = (q->IRootTwo / (2.0f * q->A));
+		result = 0.0f;
+	}
+
+	result = ((-1.0f * q->B) - result);
+	result = (result / (2.0f * q->A));
+
+	return result;
+}
+
+
+inline void
+PrettyPrint(QuadEquation *q)
+{
+	if (q->IRootOne > 0.0f) 
+	{
+		printf("Root One: %.2f + %.2fi\n", q->RootOne, q->IRootOne);
+	}
+	else
+	{
+		printf("Root One: %.2f\n", q->RootOne);
+	}
+
+	if (q->IRootTwo > 0.0f)
+	{
+		printf("Root Two: %.2f - %.2fi\n", q->RootTwo, q->IRootTwo);
+	}
+	else
+	{
+		printf("Root One: %.2f\n", q->RootTwo);
+	}
 }
