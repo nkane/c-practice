479547b9587a6b16944649ace92f5eb362509fea
Nick Kane <nkanedev@gmail.com>
2017-05-03 19:57:39 -0400

    completed day 37!

--- day-037/problem-1/main.cpp
+++ day-037/problem-1/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Write and run a C program that accepts six Fahrenheit temperatures, one at a time,
- * and converts each value entered to its Celsius  equivalent before the next value is
+ * and converts each value entered to its Celsius equivalent before the next value is
  * requested. Use a for loop in your program. The conversion required is:
  *
  * Celsius = (5.0 / 9.0) * (Fahrenheit - 32.0)
@@ -9,8 +9,63 @@
 
 #include <stdio.h>
 
+
+#define global_variable static
+
+global_variable int Total_Loop = 6;
+global_variable float Compute_Value = (5.0f / 9.0f);
+global_variable float Offset_Value = 32.0f;
+
+struct Temperature
+{
+	float F;
+	float C;
+};
+
+inline Temperature
+CreateTemperature();
+
+inline float
+ComputeCelsiusT(Temperature *);
+
+inline void
+PrettyPrint(Temperature *);
+
 int
 main()
 {
+	Temperature T = CreateTemperature();
+
+
+	for (int i = 0; i < Total_Loop; ++i)
+	{
+		printf("Enter in a valid fahrenheit temp: \n");
+		scanf("%f", &T.F);
+		T.C = ComputeCelsiusT(&T);
+		PrettyPrint(&T);
+	}
+
 	return 0;
+}
+
+
+inline Temperature
+CreateTemperature()
+{
+	Temperature T = {};
+	return T;
+}
+
+inline float
+ComputeCelsiusT(Temperature *t)
+{
+	float result = ((Compute_Value) * (t->F - Offset_Value));
+	return result;
+}
+
+inline void 
+PrettyPrint(Temperature *t)
+{
+	printf("F-Temp\t C-Temp\n");
+	printf("%.2f\t%2.f\n", t->F, t->C);
 }
--- day-037/problem-2/main.cpp
+++ day-037/problem-2/main.cpp
@@ -7,8 +7,60 @@
 
 #include <stdio.h>
 
+#define global_variable static
+
+global_variable int Loop_Amount = 10;
+global_variable float Liters_To_Gallons = 3.785f;
+
+struct LiquidMeasurement
+{
+	float Gallons;
+	float Liters;
+};
+
+inline LiquidMeasurement
+CreateLiquidMeasurement();
+
+inline float
+ComputeLitersLM(LiquidMeasurement *);
+
+inline void
+PrettyPrint(LiquidMeasurement *);
+
 int
 main()
 {
+	LiquidMeasurement LM = CreateLiquidMeasurement();
+	
+	for (int i = 0; i < Loop_Amount; ++i)
+	{
+		printf("Enter in gallon amount:\n");
+		scanf("%f", &LM.Gallons);
+		LM.Liters = ComputeLitersLM(&LM);
+		PrettyPrint(&LM);
+	}
+
 	return 0;
 }
+
+inline LiquidMeasurement
+CreateLiquidMeasurement()
+{
+	LiquidMeasurement LM = {};
+	return LM;
+}
+
+inline float
+ComputeLitersLM(LiquidMeasurement *lm)
+{
+	float result = (lm->Gallons * Liters_To_Gallons);
+	return result;
+}
+
+inline void
+PrettyPrint(LiquidMeasurement *lm)
+{
+	printf("Gallons\tLiters\n");
+	printf("%.2f\t%.5f\n", lm->Gallons, lm->Liters);
+}
+
