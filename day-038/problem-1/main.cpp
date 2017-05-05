d1aab4d89df9506018cf8f2667aab31e9052c5a1
Nick Kane <nkanedev@gmail.com>
2017-05-04 20:13:28 -0400

    set up day 38 files

---
+++ day-038/problem-1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Write and run a C program that calculates and displays the amount of money available in
+ * a bank account that initially has $1,000 deposited in it and earns 8 percent interest a year.
+ * Your program should display the amount available at the end of each year for a period of
+ * 10 years. Use the relationship that the money available at the end of each of year equals the
+ * amount of money in the account at the start of the year plus .08 times the amount available
+ * at the start of the year.
+ *
+ */
+
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
---
+++ day-038/problem-2/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * A machine purchased for $28,000 is depreciated at a rate of $4,000 a year for seven years.
+ * Write and run a program that computes and displays a depreciation table for seven years.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
