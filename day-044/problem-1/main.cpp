652a847c2fbc3908a87adcb1ebbdd0007b9c1d95
Nick Kane <nkanedev@gmail.com>
2017-05-13 10:02:21 -0400

    adding day 44 files

---
+++ day-044/problem-1/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *  A bowling team consists of five players. Each player bowls three games. Write a C
+ *  program that uses a nested loop to enter each player's individual scores and then computes
+ *  and displays the average score for each bowler. Assume that each bowler has the following scores:
+ *
+ * 1st bowler: 286 252 265
+ * 2nd bowler: 212 186 215
+ * 3rd bowler: 252 232 216
+ * 4th bowler: 192 201 235
+ * 5th bowler: 186 236 272
+ *  
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
