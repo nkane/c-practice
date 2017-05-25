d154a5b8416b302a3c2a7aed165ddf3b1f352ac3
Nick Kane <nkanedev@gmail.com>
2017-05-25 18:32:18 -0400

    completed day 51

--- day-051/problem-1/main.cpp
+++ day-051/problem-1/main.cpp
@@ -1,7 +1,87 @@
+/*
+ * Write a function named conversions() that displays a table of miles converted to
+ * kilometers. The function should accept the initial mile value to be converted, the
+ * number of conversion to be made, and the increment between each mile value. For each
+ * conversion the function should call a function named milesToKm() that accepts a
+ * mle balue and returns its equivalent kilometer value. In computing the conversion,
+ * use the relationship that there are 1.6093 kilometers per mile.
+ *
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+
+#define global_variable static
+
+global_variable const float Kilometers_Per_Mile = 1.6093f;
+
+struct Distance
+{
+	float Miles;
+	float Kilometers;
+};
+
+inline Distance *
+CreateDistance();
+
+inline float 
+ComputeKilometersFromMiles(float);
+
+inline void
+DestoryDistance(Distance *);
 
 int
 main()
 {
+	int increments = 0;
+	float incrementStep = 0;
+
+	Distance * D = CreateDistance();
+
+	printf("Enter initial mile value:\n");
+	scanf("%f", &D->Miles);
+
+	printf("Enter number of conversion:\n");
+	scanf("%d", &increments);
+
+	printf("Enter value of increments:\n");
+	scanf("%f", &incrementStep);
+
+	for (int i = 0; i < increments; ++i)
+	{
+
+		D->Kilometers = ComputeKilometersFromMiles(D->Miles);
+
+		printf("Miles: %.2f\t", D->Miles);
+		printf("Kilometers: %.2f\n", D->Kilometers);
+
+		D->Miles += incrementStep;
+	}
+
+	DestoryDistance(D);
 	return 0;
 }
+
+inline Distance *
+CreateDistance()
+{
+	Distance * D = (Distance *)malloc(sizeof(Distance));
+	return D;
+}
+
+inline float 
+ComputeKilometersFromMiles(float m)
+{
+	return (m * Kilometers_Per_Mile);
+}
+
+inline void
+DestoryDistance(Distance *d)
+{
+	if (d)
+	{
+		free(d);
+	}
+}
+
+
--- day-051/problem-2/main.cpp
+++ day-051/problem-2/main.cpp
@@ -1,7 +1,50 @@
+/*
+ * A common statistical problem is to determin how many ways n objects can be
+ * selected from a group of m objects. The number of such possibilities is given by
+ * the formula:
+ *
+ * m! / n! (m - n)!
+ *
+ * m = 8
+ * n = 3
+ *
+ * (8 * 7 * 6 * 5 * 4 * 3 * 2 * 1) / (3 * 2 * 1) (5 * 4 * 3 * 2 * 1) = ~56
+ *
+ */
+
 #include <stdio.h>
+
+inline float
+MultiplyNTimes(float);
 
 int
 main()
 {
+	float n = 0.0f, m = 0.0f;
+
+	printf("Enter in a value for n:\n");
+	scanf("%f", &n);
+
+	printf("Enter in a value for m:\n");
+	scanf("%f", &m);
+
+	float topResult = MultiplyNTimes(m);
+	float bottomResult = (MultiplyNTimes(n) * MultiplyNTimes((m - n)));
+	float result = topResult / bottomResult;
+
+	printf("Result: %.2f\n", result);
+
 	return 0;
 }
+
+inline float
+MultiplyNTimes(float n)
+{
+	if (n > 0)
+	{ 
+		return n * MultiplyNTimes(n - 1);
+	}
+
+	return 1;
+}
+
