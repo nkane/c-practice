50d0d258cc05fece34cff98d96d3711e44a54324
Nick Kane <nkanedev@gmail.com>
2017-06-05 20:26:33 -0400

    completed day 54?

--- day-054/problem-1/main.cpp
+++ day-054/problem-1/main.cpp
@@ -1,8 +1,35 @@
+/*
+ * Write a function named winPercent() that accepts the number of wins and losses
+ * that a team achieves and returns its winning percentage. Use the formula:
+ *
+ * 	- winning percentage = 100 * wins / (wins + losses)
+ *
+ */
+
 #include <stdio.h>
+
+inline float
+WinPercentage(float, float);
 
 int
 main()
 {
+	float wins = 0.0f, losses = 0.0f; 
+
+	printf("Enter in the number of wins:\n");
+	scanf("%f", &wins);
+
+	printf("Enter in the number of losses:\n");
+	scanf("%f", &losses);
+
+	printf("Win Percentage: %.2f\n", WinPercentage(wins, losses));
+
 	return 0;
+}
+
+inline float
+WinPercentage(float w, float l)
+{
+	return (100.0f * w / (w + l));
 }
 
--- day-054/problem-2/build.bat
+++
@@ -1,7 +0,0 @@
-IF NOT EXIST .\build mkdir .\build
-pushd .\build
-
-cl -Od -MTd -Zi -nologo ..\main.cpp
-
-popd
-
