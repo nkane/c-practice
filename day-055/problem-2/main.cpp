6b1e02946c270864691d098d2e01776463260fb5
Nick Kane <nkanedev@gmail.com>
2017-06-11 11:30:12 -0400

    completed problem 2 day 55

--- day-055/problem-2/main.cpp
+++ day-055/problem-2/main.cpp
@@ -1,7 +1,30 @@
+/*
+ * Write a C program named whole() the returns the integer part of any number passed to the function.
+ * (Hint: assign thet passed argument to an integer variable.)
+ *
+ */
+
 #include <stdio.h>
+
+inline int
+Whole(float);
 
 int
 main()
 {
+	float value = 0.0f;
+	
+	printf("Enter in a float value:\n");
+	scanf("%f", &value);
+
+	printf("Whole value: %d\n", Whole(value));
+
 	return 0;
 }
+
+inline int
+Whole(float value)
+{
+	return ((int)(value));
+}
+
--- day-055/problem-3/main.cpp
+++ day-055/problem-3/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Write a C program named fracpract() that returns the fractional part of any
+ * number passed to the function. For example, if thenumber 256.879 is passed to
+ * fracpart(), then umber .879
+ *
+ */
+
+
 #include <stdio.h>
 
 int
