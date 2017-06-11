ab55bde30a3454b2db9d0838223631070dbede83
Nick Kane <nkanedev@gmail.com>
2017-06-11 11:36:26 -0400

    completed all problems for day 55

--- day-055/problem-3/main.cpp
+++ day-055/problem-3/main.cpp
@@ -1,15 +1,32 @@
 /*
  * Write a C program named fracpract() that returns the fractional part of any
  * number passed to the function. For example, if thenumber 256.879 is passed to
- * fracpart(), then umber .879
+ * fracpart(), then umber .879 should be returned.
  *
  */
 
-
 #include <stdio.h>
+
+inline float
+FractionalPart(float);
 
 int
 main()
 {
+	float value = 0.0f;
+
+	printf("enter in a float value:\n");
+	scanf("%f", &value);
+
+	printf("Fractional Part: %.6f\n", FractionalPart(value));
+
 	return 0;
+}
+
+inline float
+FractionalPart(float value)
+{
+	float result = 0.0f;
+	result = (value - (int)(value));
+	return result;
 }
