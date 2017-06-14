ce3a1c2009453c7c4358445bf52ae4b065077bc0
Nick Kane <nkanedev@gmail.com>
2017-06-13 20:22:50 -0400

    completed problem 2 - day 57 completed

--- day-057/problem-2/main.cpp
+++ day-057/problem-2/main.cpp
@@ -1,7 +1,31 @@
+/*
+ * Write a C function named root4() that returns the fourth root of the argument passed to it.
+ * 
+ */
+
 #include <stdio.h>
+#include <math.h>
+
+inline float
+FourthRoot(float);
 
 int
 main()
 {
+	float value = 0.0f;
+	printf("Enter in a value:\n");
+	scanf("%f", &value);
+
+	printf("Fourth Root: %.2f\n", FourthRoot(value));
+
 	return 0;
 }
+
+inline float
+FourthRoot(float value)
+{
+	float result = 0.0f;
+	result = sqrt(sqrt(value));
+	return result;
+}
+
