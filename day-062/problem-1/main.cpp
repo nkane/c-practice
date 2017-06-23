582ca8815ed15230613bde15657b589b37e08d2b
Nick Kane <nkanedev@gmail.com>
2017-06-22 22:33:11 -0400

    completed problem for day 62

--- day-062/problem-1/main.cpp
+++ day-062/problem-1/main.cpp
@@ -6,14 +6,131 @@
  * not exceed 21, else it is counted as a 1. Using this information write a C program that uses
  * a random number generator to select three cards (a 1 initially corresponding to an ace,
  * a 2 corresponding to a face card of two, and so on), calculate the total value of the 
- * hand appropriately, and display the value of the three cards with a printed message.c:w
+ * hand appropriately, and display the value of the three cards with a printed message.
  *
  */
 
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define global_varaible static
+#define array_len(array) (sizeof(array)/sizeof(array[0]))
+
+enum Cards
+{
+	None         = 0,
+	Ace   	     = 1, 
+	FaceCard     = 10,
+	AceSecondary = 11,
+};
+
+global_varaible const int PlayerIndex = 1;
+global_varaible const int MaxValue = 21;
+
+global_varaible bool IsPlaying = true;
+global_varaible int Hand[3] =
+{
+	0,
+	0,
+	0,
+};
+
+void
+RunGame();
+
+int
+GenerateCardValue();
+
+bool
+IsHandABust(int);
+
+bool
+IsWinningHand(int);
 
 int
 main()
 {
+	srand(time(NULL));
+	char c;
+	int cardValue = 0;
+	
+	while (IsPlaying)
+	{
+		RunGame();
+		printf("Continue playing? (y)\n");
+		scanf("\n%c", &c);
+		if (tolower(c) != 'y')
+		{
+			IsPlaying = false;
+		}
+	}
+
 	return 0;
 }
+
+void
+RunGame()
+{
+	bool DrawAce = false;
+	int sum = 0;
+	for (int i = 0; i < array_len(Hand); ++i)
+	{
+		Hand[i] = 0;
+		int cardValue = GenerateCardValue();
+		if (cardValue == Ace)
+		{
+			DrawAce = true;
+			Hand[i] += AceSecondary;
+		}
+		else 
+		{
+			Hand[i] += GenerateCardValue();
+		}
+		sum += Hand[i];
+	}
+
+	if (IsHandABust(sum))
+	{
+		if (DrawAce)
+		{
+			printf("swapping ace values to avoid busting!\n");
+			sum -= AceSecondary;
+			sum += Ace;
+		}
+		else
+		{
+			printf("Sorry - you busted!\n");
+		}
+	}
+	else if (IsWinningHand(sum))
+	{
+		printf("Congrats you won!\n");
+	}
+
+	printf("Hand Value: %d\n", sum);
+}
+
+int
+GenerateCardValue()
+{
+	int cardValue = 0;
+	cardValue = (Ace + (rand() % FaceCard));
+	return (cardValue);
+}
+
+bool
+IsHandABust(int handValue)
+{
+	bool isBust = (handValue > MaxValue) ? true : false;
+	return (isBust);
+}
+
+bool
+IsWinningHand(int handValue)
+{
+	bool isWinner = (handValue == MaxValue) ? true : false;
+	return (isWinner);
+}
+
