d4d887fa8f801c6290aea593ad76d11717397ce3
Nick Kane <nkanedev@gmail.com>
2017-06-23 22:07:19 -0400

    completed day 63 problem and moved problem to day 64

--- day-063/problem-1/main.cpp
+++ day-063/problem-1/main.cpp
@@ -12,10 +12,135 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define array_len(array)(sizeof(array)/sizeof(array[0]))
+
+#define global_varaible static
+
+global_varaible const int Min = 97;
+global_varaible const int Max = 122;
+global_varaible bool IsGenerating = true;
+global_varaible char CurrentLetters[3];
+
+int 
+GenerateNumber(int, int);
+
+bool 
+ValidString(char *);
 
 int
 main()
 {
+	int count = 0;
+	srand(time(NULL)); 
+	while (IsGenerating)
+	{
+		for (int i = 0; i < array_len(CurrentLetters); ++i)
+		{
+			if ((i + 1) == array_len(CurrentLetters)) 
+			{
+				CurrentLetters[i] = '\0';
+			}
+			else 
+			{
+				CurrentLetters[i] = (char)GenerateNumber(Min, Max);
+			}
+		}
+
+		printf("Generated: %s\n", CurrentLetters);
+
+		IsGenerating = !ValidString(CurrentLetters);
+		count += 2;
+	}
+
+	printf("\nTotal Letters Types: %d\n", count);
+	printf("Found Word: %s\n", CurrentLetters);
+
+	system("pause");
+
 	return 0;
+}
+
+int 
+GenerateNumber(int min, int max)
+{
+	int result = 0;
+	result = rand();
+	result %= max;
+	if (result < min)
+	{
+		result = GenerateNumber(min, max);
+	}
+	return (result);
+}
+
+bool 
+ValidString(char *string)
+{
+	switch (*string)
+	{
+		case 'a':
+		{
+			++string;
+			if (*string == 't')
+			{
+				return true;
+			}
+		} break;
+
+		case 'i':
+		{
+			++string;
+			if (*string == 's')
+			{
+				return true;
+			}
+		} break;
+
+		case 'h':
+		{
+			++string;
+			if (*string == 'e')
+			{
+				return true;
+			}
+		} break;
+		
+		case 'w':
+		{
+			++string;
+			if (*string == 'e')
+			{
+				return true;
+			}
+		} break;
+
+		case 'u':
+		{
+			++string;
+			if (*string == 'p')
+			{
+				return true;
+			}
+		} break;
+
+		case 'o':
+		{
+			++string;
+			if (*string == 'n')
+			{
+				return true;
+			}
+		} break;
+
+		default:
+		{
+
+		} break;
+	}
+
+	return false;
 }
 
