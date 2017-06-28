2d44e6ba5ca3a7d5a91bc32fafa314f2805bb0db
Nick Kane <nkanedev@gmail.com>
2017-06-27 22:17:07 -0400

    completed day 67 problems

--- day-067/problem-2/main.cpp
+++ day-067/problem-2/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Write a C function named change() that accepts a single-precision number
- * and the address of the integer varaibles named quarters, dimes, nickels,
+ * and the address of the integer variables named quarters, dimes, nickels,
  * and pennies. The function should determine the number of quarters, dimes,
  * nickels, and pennies passed to it and write these values directly into
  * the respective variables declared in its calling function.
@@ -9,9 +9,53 @@
 
 #include <stdio.h>
 
+void
+Change(int, int *, int *, int*, int *);
+
 int
 main()
 {
+	float moneyValue = 0.0f;
+	int quarters = 0, dimes = 0, nickels = 0, pennies = 0;
+
+	printf("Enter in a single precision money value:\n");
+	scanf("%f", &moneyValue);
+
+	Change(((int)(moneyValue * 100.0f)), &quarters, &dimes, &nickels, &pennies);
+
+	printf("Quarters: %d\n", quarters);
+	printf("Dimes: %d\n", dimes);
+	printf("Nickels: %d\n", nickels);
+	printf("Pennies: %d\n", pennies);
+
 	return 0;
+}
+
+void
+Change(int value, int *q, int *d, int *n, int *p)
+{
+	while (value > 0)
+	{
+		if (value >= 25)
+		{
+			(*q) += 1;
+			value -= 25;
+		}
+		else if (value >= 10)
+		{
+			(*d) += 1;
+			value -= 10;
+		}
+		else if (value >= 5)
+		{
+			(*n) += 1;
+			value -= 5;
+		}
+		else 
+		{
+			(*p) += 1;
+			value -= 1;
+		}
+	}
 }
 
--- day-067/problem-3/main.cpp
+++ day-067/problem-3/main.cpp
@@ -1,19 +1,55 @@
 /*
- * Write a function named secs() that accepts the time in hours, minutes, 
+ * Write a function named seconds() that accepts the time in hours, minutes, 
  * and seconds; and determine the total number of seconds in the passed data. 
  * Write this function so that the total number of seconds is returned by the
  * function as an integer number.
  *
  * Create a second function that passes by reference of the variable that it
- * will operate directly on as well as the other values.
+ * will operate directly on as well as the other values instead of a return value.
  *
  */
 
 #include <stdio.h>
 
+inline
+int SecondsByValue(int, int, int);
+
+inline
+void SecondsByReference(int *, int, int, int);
+
 int
 main()
 {
+	int hours = 0, minutes = 0, seconds = 0;
+	int time_value = 0, time_ptr = 0;
+
+	printf("Enter in the hours: ");
+	scanf("%d", &hours);
+
+	printf("Enter in the minutes: ");
+	scanf("%d", &minutes);
+	
+	printf("Enter in the seconds: ");
+	scanf("%d", &seconds);
+
+	time_value = SecondsByValue(hours, minutes, seconds);
+	SecondsByReference(&time_ptr, hours, minutes, seconds);
+
+	printf("time_value: %d\n", time_value);
+	printf("time_ptr: %d\n", time_ptr);
+
 	return 0;
+}
+
+inline
+int SecondsByValue(int hours, int minutes, int seconds)
+{
+	return (((hours * 60) * 60) + (minutes * 60) + seconds);
+}
+
+inline
+void SecondsByReference(int *value_ptr, int hours, int minutes, int seconds)
+{
+	(*value_ptr) = SecondsByValue(hours, minutes, seconds);
 }
 
