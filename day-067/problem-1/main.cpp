15f6fdbb244404a6b8dc48c9b3ae6319b7e13195
Nick Kane <nkanedev@gmail.com>
2017-06-27 21:45:54 -0400

    completed problem 1 for day 67

--- day-067/problem-1/main.cpp
+++ day-067/problem-1/main.cpp
@@ -10,8 +10,9 @@
  *	double price;
  *
  * Display the amount of storage your computer reserves for each data type (use the sizeof
- * operator). With this information, determine if your computer set aside storage for the variables
- * in the order they were declared.
+ * operator). With this information, determine if your computer sets aside storage for the
+ * variables in the order they were declared.
+ *
  */
 
 #include <stdio.h>
@@ -19,6 +20,29 @@
 int
 main()
 {
+	char key, choice;
+	int num, count;
+	long date;
+	float yield;
+	double price;
+
+	// char size - 1 byte (8 bits)
+	printf("key: %c    - address: 0x%x - sizeof: %d\n", key, (int)&key, sizeof(key));
+	printf("choice: %c - address: 0x%x - sizeof: %d\n\n", choice, (int)&choice, sizeof(choice));
+
+	// int size - 4 bytes (32 bits)
+	printf("num: %d   - address: 0x%x - sizeof: %d\n", num, (int)&num, sizeof(num));
+	printf("count: %d - address: 0x%x - sizeof: %d\n\n", count, (int)&count, sizeof(count));
+
+	// long size - 8 bytes (64 bits)
+	printf("date: %ld - address: 0x%x - sizeof: %d\n\n", date, (int)&date, sizeof(date));
+	
+	// float size - 4 bytes (32 bits)
+	printf("date: %f - address: 0x%x - sizeof: %d\n\n", yield, (int)&yield, sizeof(yield));
+	
+	// double size - 8 bytes (64 bits)
+	printf("price: %e - address: 0x%x - sizeof: %d\n\n", price, (int)&price, sizeof(price));
+
 	return 0;
 }
 
