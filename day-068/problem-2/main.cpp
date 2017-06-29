dffbd730176d936a9cfb9a95ad7f1566b754d053
Nick Kane <nkanedev@gmail.com>
2017-06-28 22:19:13 -0400

    completed day 68 problems

--- day-068/problem-2/main.cpp
+++ day-068/problem-2/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Replace the calc() function used in Program 7.8 with two functions named
- * computeSUm() and computeProduct(). The computeSum() function should calculate
+ * computeSum() and computeProduct(). The computeSum() function should calculate
  * and directly return the sum of three values passed to it, while the
  * computeProduct() function should calculate and directly return the product
  * of three values passed to it.
@@ -9,9 +9,58 @@
 
 #include <stdio.h>
 
+// replacing with two function calls
+/*
+void
+calc(float, float, float, float *, float *);
+*/
+
+float
+ComputeSum(float, float, float);
+
+float
+ComputeProduct(float, float, float);
+
 int
 main()
 {
+	float firstNumber, secondNumber, thirdNumber, sum, product;
+
+	printf("Enter in three numbers: " );
+	scanf("%f %f %f", &firstNumber, &secondNumber, &thirdNumber);
+	
+	sum = ComputeSum(firstNumber, secondNumber, thirdNumber);
+	product = ComputeProduct(firstNumber, secondNumber, thirdNumber);
+
+	// replacing with two function calls
+	//calc(firstNumber, secondNumber, thirdNumber, &sum, &product);
+
+	printf("\nThe sum of the entered numbers is: %6.2f", sum);
+	printf("\nThe product of the entered numbers is: %6.2f", product);
+
 	return 0;
+}
+
+// replacing with two function calls
+/*
+void
+calc(float n1, float n2, float n3, float *sumAddress, float *productAddress)
+{
+	*sumAddress = n1 + n2 + n3;
+	*productAddress = n1 * n2 * n3;
+}
+*/
+
+float
+ComputeSum(float n1, float n2, float n3)
+{
+	return (n1 + n2 + n3);
+}
+
+
+float
+ComputeProduct(float n1, float n2, float n3)
+{
+	return (n1 * n2 * n3);
 }
 
--- day-068/problem-3/main.cpp
+++ day-068/problem-3/main.cpp
@@ -4,7 +4,7 @@
  * day, and year, and returns these three values to the calling function.
  * For example, if date is called using the statement
  *
- * 	date(20120411, &month, &day, &year);
+ * 	date(20120412, &month, &day, &year);
  *
  * the number 4 should be returned in month, the number 11 in day, and
  * the number 2012 in year.
@@ -13,9 +13,28 @@
 
 #include <stdio.h>
 
+void
+Date(int date, int *month, int *day, int *year);
+
 int
 main()
 {
+	int date = 20070412;
+	int month, day, year;
+
+	Date(date, &month, &day, &year);
+
+	printf("Month: %d\n", month);
+	printf("Day: %d\n", day);
+	printf("Year: %d\n", year);
+
 	return 0;
 }
 
+void
+Date(int date, int *month, int *day, int *year)
+{
+	(*year) = (date / 10000);
+	(*month) = ((date - (*year) * 10000) / 100);
+	(*day) = (((date - (*year) * 10000)) - (*month * 100));
+}
