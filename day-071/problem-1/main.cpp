fbbdf8f0e81efb5af675e5749f171f753b5f1924
Nick Kane <nkanedev@gmail.com>
2017-07-01 10:23:13 -0400

    adding set up files for day 71

---
+++ day-071/problem-1/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * The Fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13 ...
+ * where the first two terms are 0 and 1, and each term
+ * thereafter is defined recursively as the sum of two
+ * preceding terms; that is
+ *
+ * Fib(n) = n for n < 2
+ * Fib(n) = Fib(n - 1) + Fib(n - 2) for n >= 2
+ *
+ * Write a recursive function that returns the nth number in
+ * the Fibonacci sequence when n is passed to the function as
+ * an argument. For example, when n = 8, the function returns
+ * the 8th number in the sequence, which is 13.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
---
+++ day-071/problem-3/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Write a function that recursively determines the value
+ * of the nth term of an arithmetic sequence defined by the
+ * terms
+ *
+ * 	a, a + d, a + 2d, a + 3d, ..... a + (n - 1)d
+ *
+ * The argument to the function should be the first term, a,
+ * the common difference, d, and the value of n.
+ *
+ */
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
+
