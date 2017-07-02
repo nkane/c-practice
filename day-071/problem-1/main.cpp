4ca30b50020c74859938b5a97b8230d1c6a34a32
Nick Kane <nkanedev@gmail.com>
2017-07-02 13:03:03 -0400

    completed problem for day 71

--- day-071/problem-1/main.cpp
+++ day-071/problem-1/main.cpp
@@ -17,7 +17,68 @@
 #include <stdio.h>
 
 int
+FibRecursive(int n);
+
+int
+FibIterative(int n);
+
+int
 main()
 {
-	return 0;
+	int n = 0;
+	printf("Enter in n: ");
+	scanf("%d", &n);
+
+	printf("FibRecursive(n): %d\n", FibRecursive((n - 1)));
+	printf("FibIterative(n): %d\n", FibIterative((n - 1)));
+
+	return (0);
 }
+
+int
+FibRecursive(int n)
+{
+	int x = 0;
+	int y = 0;
+	int result = 0;
+	if (n >= 2)
+	{
+		x = FibRecursive(n - 1);
+		y = FibRecursive(n - 2);
+		result = x + y;
+		printf("fib -> %d\n", result);
+		return (x + y);
+	}
+	// return 1 or 0
+	return n;
+}
+
+int
+FibIterative(int n)
+{
+	int firstN = 0;
+	int secondN = 1;
+	int next = 0;
+
+	/*
+	 * 1 + 0 = 1
+	 * 1 + 1 = 2
+	 * 1 + 2 = 3
+	 * 2 + 3 = 5
+	 * 3 + 5 = 8
+	 * 5 + 8 = 13
+	 * . . .
+	 */
+	for (int i = 0; i <= n; ++i)
+	{
+		if (i > 1)
+		{
+			next = firstN + secondN;
+			firstN = secondN;
+			secondN = next;
+		}
+	}
+	
+	return (next);
+}
+
