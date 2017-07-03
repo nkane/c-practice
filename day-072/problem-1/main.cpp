d15171a66c214d93e491949e67ace73a46ec8038
Nick Kane <nkanedev@gmail.com>
2017-07-03 19:36:19 -0400

    adding day 72 problem definitions

--- day-072/problem-1/main.cpp
+++ day-072/problem-1/main.cpp
@@ -1,3 +1,15 @@
+/*
+ * The value of x^n can be defined recursively as:
+ * 	x^0 = 1
+ * 	x^n = x * x^n-1
+ *
+ * Write a function that returns the computes and returns the value of
+ * x^n.
+ *
+ * Write the iterative version of this function as well.
+ *
+ */
+
 #include <stdio.h>
 
 int
--- day-072/problem-2/main.cpp
+++ day-072/problem-2/main.cpp
@@ -1,3 +1,16 @@
+/*
+ * The following algorithm, discovered by Euclid, provides a simple means
+ * of determining the greatest common divisor (GCD) of two positive integers a and b
+ * (the GCD is the largest number that divides evenly into both numbers with no remainder):
+ * 	a) divide the larger number by the smaller and retain the remainder
+ * 	b) divide the smaller number by the remainder, again retaining the remainder
+ * 	c) continue dividing the prior remainder by the current remainder until the 
+ * 	   remainder is 0, at which point the last non-0 remainder is the GCD.
+ *
+ * Write a recursive function named gcd() that implements this algorithm.
+ *
+ */
+
 #include <stdio.h>
 
 int
