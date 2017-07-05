ae266bf591bab0e7904b139bb7f2377591eadc84
Nick Kane <nkanedev@gmail.com>
2017-07-04 22:15:50 -0400

    completed day 72

--- day-072/problem-1/main.cpp
+++ day-072/problem-1/main.cpp
@@ -11,10 +11,60 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int
+PowerRecursive(int x, int n);
+
+int
+PowerIterative(int x, int n);
 
 int
 main()
 {
-	return 0;
+	int x = 0, n = 0;
+
+	printf("Enter in x: ");
+	scanf("%d", &x);
+
+	printf("Enter in n: ");
+	scanf("%d", &n);
+
+	printf("Recursive: %d\n", PowerRecursive(x, n)); 
+	printf("Iterative: %d\n", PowerIterative(x, n));
+
+	system("pause");
+
+	return (0);
+}
+
+int
+PowerRecursive(int x, int n)
+{
+	if (n == 0)
+	{
+		return 1;
+	}
+	return x * PowerRecursive(x, (n - 1));
+}
+
+int
+PowerIterative(int x, int n)
+{
+	int result = 0;
+	if (n == 0) 
+	{
+		result = 1;
+	}
+	else 
+	{
+		result = x;
+	}
+
+	for (int i = 1; i < n; ++i) 
+	{
+		result *= x;
+	}
+	return (result);
 }
 
--- day-072/problem-2/main.cpp
+++ day-072/problem-2/main.cpp
@@ -14,8 +14,79 @@
 #include <stdio.h>
 
 int
+GCDRecursive(int larger, int smaller);
+
+void
+XorSwap(int *n1, int *n2);
+
+int
 main()
 {
+	int n1 = 0, n2 = 0;
+	printf("Enter in value 1: \n");
+	scanf("%d", &n1);
+
+	printf("Enter in value 2: \n");
+	scanf("%d", &n2);
+
+	if (n2 > n1)
+	{
+		XorSwap(&n1, &n2);
+	}
+
+	GCDRecursive(n1, n2);
+
 	return 0;
+}
+
+// larger: 468
+// smaller: 24
+// a) 468 % 24 = 12
+// b) 24 % 12 = 0
+// c) none ...
+// return 12
+// got close to solving this one, but needed some help from:
+// http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
+int
+GCDRecursive(int larger, int smaller)
+{
+	if ((larger % smaller) == 0)
+	{
+		return smaller;
+	}
+	return GCDRecursive(smaller, (larger % smaller));
+}
+
+void
+XorSwap(int *n1, int *n2)
+{
+	// example
+	// n1 = 0001
+	// n2 - 0011
+	
+	// n1 ^= n2
+	//  0001
+	// ^0011
+	// -----
+	//  0010
+	//  n1 = 0010
+	
+	// n2 ^= n1
+	//  0011
+	// ^0010
+	// -----
+	//  0001
+	//  n2 = 0001
+
+	// n1 ^= n2
+	//  0010
+	// ^0001
+	// -----
+	//  0011
+	//  n1 = 0011
+
+	*n1 ^= *n2;
+	*n2 ^= *n1;
+	*n1 ^= *n2;
 }
 
