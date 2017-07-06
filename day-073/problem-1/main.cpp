38814ce04d8d2e901e1936ec1015206240c3f163
Nick Kane <nkanedev@gmail.com>
2017-07-05 21:53:19 -0400

    completed day 73

--- day-073/problem-1/main.cpp
+++ day-073/problem-1/main.cpp
@@ -8,10 +8,53 @@
  */
 
 #include <stdio.h>
+#include <math.h>
+
+int
+isPalindrome(int num);
+
+int 
+reverse(int num);
 
 int
 main()
 {
+	int x = 4321234;
+	int y = 1034;
+
+	int testOne = isPalindrome(x);
+	int testTwo = isPalindrome(y);
+
 	return 0;
 }
 
+
+// NOTE(nick): was stumped again on this problem
+// used following source for help / source code:
+// http://www.codeforwin.in/2016/03/c-program-to-check-palindrome-number-using-recursion.html
+
+int
+isPalindrome(int num)
+{
+	if (num == reverse(num))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int 
+reverse(int num)
+{
+	int digit;
+	
+	if (num == 0)
+	{
+		return 0;
+	}
+
+	// total number of digits
+	digit = (int)log10(num);
+
+	return (num % 10 * pow(10, digit) + reverse(num / 10));
+}
