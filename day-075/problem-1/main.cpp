bf5fa15ad138d294977b150c1e3f80a1bdc2ea26
Nick Kane <nkanedev@gmail.com>
2017-07-07 19:35:38 -0400

    adding problem descriptions for day 75

--- day-075/problem-1/main.cpp
+++ day-075/problem-1/main.cpp
@@ -1,5 +1,13 @@
 /*
+ * Write a C program that declares three one-dimensional arrays named price,
+ * quantity, and amount. Each array should be capable of holding 10 elements.
+ * Using a for loop, input values for the price and quantity arrays. The
+ * entries in the amount array should be the product of the corresponding
+ * values in the price and quantity arrays (thus, amount[i] = quantity[i] * price[i];).
+ * After all of the data has been entered display the following output:
  *
+ * 	Quantity Price Amount
+ * 	-------- ----- ------
  *
  */
 
--- day-075/problem-2/main.cpp
+++ day-075/problem-2/main.cpp
@@ -1,5 +1,16 @@
 /*
+ * Write a program that inputs 10 double-percision numbers into an array named raw.
+ * After 10 user-input numbers are entered into the array, your program should cycle
+ * through raw 10 times. During each pass through the array, your program should
+ * select the lowest value in raw and place the selected value in the next available
+ * solt in an array named sorted. Thus, when your program is complete, the sorted
+ * array should contain the numbers in raw in sorted order from lowest to highest.
+ * (Hint: make sure to reset the lowest value selected during each pass to a very high
+ * number so that it is not selected again. You will need a second for loop within
+ * the first for loop to locate the minimum value for each pass.)
  *
+ * The method used for sorting in this program is inefficient. Can you determine
+ * why? What might be a better method of sorting the numbers in an array?
  *
  */
 
