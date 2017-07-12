14775ccac0a17818dde0ade325cab692ba789fa9
Nick Kane <nkanedev@gmail.com>
2017-07-11 22:09:39 -0400

    adding setup files for day 78

---
+++ day-078/problem-1/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Write a declaration to store the following values in
+ * an array named prices: 16.24, 18.98, 23.75, 16.29, 19.54,
+ * 14.22, 11.13, 15.39. Include the declaration in a program
+ * that displays the values in the array.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
+
---
+++ day-078/problem-2/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Create a program that generates random numbers for
+ * an array of n size and does the following sort
+ * algorithms:
+ *
+ * 	1) Bubble sort
+ * 	2) Insertion sort
+ * 	3) Selection sort
+ *
+ * This exercise is for reviewing and to help promote
+ * memorizing these sorting algorithms.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
+
