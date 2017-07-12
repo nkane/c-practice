ddd1289d6941bb17ca8742fbd027f4096b7cd42a
Nick Kane <nkanedev@gmail.com>
2017-07-12 09:41:26 -0400

    adding set up files for day 79

---
+++ day-079/problem-1/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Write a C program that stores the following prices in a global
+ * array:
+ * 	- 9.92, 6.32, 12.63, 5.95, 10.29
+ *
+ * Your program should also create two automatic arrays named units
+ * and amounts, each capable of storing five double-percision numbers.
+ * Using a for loop and a scanf() function call, have the program
+ * accept five user-input numbers into the units array when the program
+ * is run. The program should store the product of the corresponding values
+ * in the prices and units array in the amounts array (for example,
+ * amountsn[i] = price[i] * units[i]) and display the following output:
+ *
+ * Price	Units		Amount
+ * -----	----		------
+ * 9.92		  *		   *
+ * ...		....		......
+ *
+ * Total: 			......
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
---
+++ day-079/problem-2/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Create a program that generates unsorted numbers for an array.
+ * Next review / implement the following sorting algorithms:
+ *
+ * 	1) Bubble Sort
+ * 	2) Insertion Sort
+ * 	3) Selection Sort
+ * 	4) Merge Sort
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
