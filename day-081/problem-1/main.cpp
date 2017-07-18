b7f050c81ce942286caf049b5a01e3c675ac3cc0
Nick Kane <nkanedev@gmail.com>
2017-07-17 20:33:07 -0400

    completed problem 1 for day 81

--- day-081/problem-1/main.cpp
+++ day-081/problem-1/main.cpp
@@ -1,7 +1,7 @@
 /*
  * Write a program that declares three one-dimensional arrays named price,
  * quantity, and amount. Each array should be delcared in main() and should
- * be capableof holding 10 double-percision numbers. The numbers that should
+ * be capable of holding 10 double-percision numbers. The numbers that should
  * be stored in price are:
  *
  * 	- 10.62, 14.89, 16.55, 18.62, 9.47, 6.58, 18.32, 12.15, 3.98
@@ -20,9 +20,48 @@
 
 #include <stdio.h>
 
+#define len(array)(sizeof(array)/sizeof(array[0]))
+
+#define global_variable static
+
+global_variable const int Size = 10;
+
+void
+Extend(double price[], double quantity[], double amount[], int size);
+
+void
+PrintArray(double price[], double quantity[], double amount[], int size);
+
 int
 main()
 {
+	double price[Size] = { 10.62, 14.89, 16.55, 18.62, 9.47, 6.58, 18.32, 12.15, 3.98, };
+	double quantity[Size]  = { 4.0, 8.5, 6, 8.35, 9, 15.3, 3, 5.4, 2.9, 4.8, };
+	double amount[Size] = { 0.0 };
+
+	Extend(price, quantity, amount, len(price));
+	PrintArray(price, quantity, amount, len(price));
+
 	return 0;
+}
+
+void
+Extend(double price[], double quantity[], double amount[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		amount[i] = (price[i] * quantity[i]);
+	}
+}
+
+void
+PrintArray(double price[], double quantity[], double amount[], int size)
+{
+	printf("Price\tQuantity\tAmount\n");
+	printf("-----\t--------\t------\n");
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%.2lf\t%.2lf\t\t%.2lf\n", price[i], quantity[i], amount[i]);
+	}
 }
 
