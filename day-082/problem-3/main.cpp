03ac20de18a9e905d63078aa43e2a7e00f35d8f6
Nick Kane <nkanedev@gmail.com>
2017-07-18 21:20:00 -0400

    completed day 82

--- day-082/problem-3/main.cpp
+++ day-082/problem-3/main.cpp
@@ -9,10 +9,59 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "main.h"
+#include "sort.h"
 
 int
 main()
 {
+	int TestCase[Size] = { 0 };
+
+	// bubble sort
+	GenerateNumbers(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (before): ");
+	BubbleSort(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (after): ");
+
+	// insertion sort
+	GenerateNumbers(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Insertion Sort (before): ");
+	InsertionSort(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Insertion Sort (after): ");
+
+	// selection sort
+	GenerateNumbers(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Selection Sort (before): ");
+	SelectionSort(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Selection Sort (after): ");
+
+	system("pause");
+
+
 	return 0;
+}
+
+void
+GenerateNumbers(int array[], int size)
+{
+	srand(time(NULL));
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = (1 + (rand() % 100));
+	}
+}
+
+void
+PrintArray(int array[], int size, char *text)
+{
+	printf("%s{ ", text);
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("}\n");
 }
 
