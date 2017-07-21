39eb0ae1ac24c7eeca4c99214e6072573912e52f
Nick Kane <nkanedev@gmail.com>
2017-07-21 01:00:15 -0400

    completed day 83

--- day-083/problem-4/main.cpp
+++ day-083/problem-4/main.cpp
@@ -9,10 +9,62 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "main.h"
+#include "sort.h"
+
+void
+GenerateNumbers(int array[], int size);
+
+void
+PrintArray(int array[], int size, char *text);
 
 int
 main()
-{
+{	
+	int TestCase[Size] = { 0 };
+
+	// bubble sort
+	GenerateNumbers(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (before):");
+	BubbleSort(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (after):");
+
+	// insertion sort
+	GenerateNumbers(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (before):");
+	InsertionSort(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (after):");
+
+	// selection sort
+	GenerateNumbers(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (before):");
+	SelectionSort(TestCase, len(TestCase));
+	PrintArray(TestCase, len(TestCase), "Bubble Sort (after):");
+
 	return 0;
+}
+
+void
+GenerateNumbers(int array[], int size)
+{
+	srand(time(NULL));
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = (1 + (rand() % 100));
+	}
+}
+
+void
+PrintArray(int array[], int size, char *text)
+{
+	printf("%s { ", text);
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("}\n");
 }
 
---
+++ day-083/problem-4/main.h
@@ -0,0 +1,10 @@
+/*
+ * main.h
+ */
+
+#define len(array) (sizeof(array)/sizeof(array[0]))
+
+#define global_variable static
+
+global_variable const int Size = 5;
+
