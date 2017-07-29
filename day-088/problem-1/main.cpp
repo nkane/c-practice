ccbd25dc31d71404b0824a9f7919d5824ff81dc9
Nick Kane <nkanedev@gmail.com>
2017-07-29 15:27:44 -0400

    adding set up files for day 88

---
+++ day-088/problem-1/main.cpp
@@ -0,0 +1,13 @@
+/*
+ *
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
+
---
+++ day-088/problem-3/build.bat
@@ -0,0 +1,8 @@
+IF NOT EXIST .\build mkdir .\build
+
+pushd .\build
+
+cl -Od -MTd -Zi ..\main.cpp
+
+popd
+
