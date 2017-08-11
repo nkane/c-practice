b3de6aa7144b958976679fdb22e4169d4d8656d8
Nick Kane <nkanedev@gmail.com>
2017-08-10 20:56:44 -0400

    adding problem definitions

--- day-091/problem-1/main.cpp
+++ day-091/problem-1/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * Modify program 8.13 to use a list of 100 randomly generated
+ * numbers and determine the number of moves required to put the
+ * list in order using a selection sort. Display both the initial
+ * list and the reordered list
+ *
+ * Implement for bubble sort as well.
+ *
+ */
+
 #include <stdio.h>
 
 int
--- day-091/problem-2/main.cpp
+++ day-091/problem-2/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * Generate numbers and implement the following sorting
+ * algorithms:
+ *
+ * 	1) bubble sort
+ * 	2) insertion sort
+ * 	3) selection sort
+ *
+ */
+
 #include <stdio.h>
 
 int
