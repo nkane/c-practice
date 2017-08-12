759e0767d64fcee87385b606c6bfdcb45284f98f
Nick Kane <nkanedev@gmail.com>
2017-08-11 22:42:08 -0400

    adding problem definitions for day 92

--- day-092/problem-1/main.cpp
+++ day-092/problem-1/main.cpp
@@ -1,5 +1,9 @@
 /*
- *
+ * For the functions selectionSort(), bubbleSort(), and quickSort(), the sorting 
+ * can be done in decreasing order by a simple modification. In each case
+ * identify the required changes and then rewrite each function to accept
+ * a flag indicating whether the sort should be in increasing or decreasing
+ * order. Modify each routine to receive and use this flag argument correctly.
  *
  */
 
--- day-092/problem-2/main.cpp
+++ day-092/problem-2/main.cpp
@@ -1,5 +1,12 @@
 /*
+ * The selection and bubble sort both use the same technique for swapping
+ * list elements. Replace the code in these two functions that performs the
+ * swap by a call to a funcito named swap. The prototype for swap should be
+ * void swap(int *, int *). The swap() function should be constructed using
+ * the algorithm presented in section 7.4
  *
+ * describe why the function quicksort() does not require the swap algorithm
+ * used by the selection and bubble sorts.
  *
  */
 
