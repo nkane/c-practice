c7897e47e1a891295eba93f113e5a850f0db9c06
Nick Kane <nkanedev@gmail.com>
2017-08-17 18:00:22 -0400

    completed problem one for day 93

--- day-093/problem-1/main.cpp
+++ day-093/problem-1/main.cpp
@@ -11,10 +11,120 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define global_variable static
+
+#define len(array)(sizeof(array)/sizeof(array[0]))
+
+global_variable const int Size = 10;
+
+void
+GenerateNumbers(int array[], int size);
+
+void
+PrintArray(int array[], int size, char *text);
+
+void
+Swap(int *x, int *y);
+
+void
+BubbleSort(int array[], int size);
+
+void
+SelectionSort(int array[], int size);
 
 int
 main()
-{
+{	
+	int TestCase[Size] = { 0 };
+
+	// bubble sort
+	{
+		GenerateNumbers(TestCase, len(TestCase));
+		PrintArray(TestCase, len(TestCase), "bubble sort (before):");
+		BubbleSort(TestCase, len(TestCase));
+		PrintArray(TestCase, len(TestCase), "bubble sort (after):");
+	}
+
+	printf("\n");
+
+	// selection sort
+	{
+		GenerateNumbers(TestCase, len(TestCase));
+		PrintArray(TestCase, len(TestCase), "selection sort (before):");
+		SelectionSort(TestCase, len(TestCase));
+		PrintArray(TestCase, len(TestCase), "selection sort (after):");
+	}
+
 	return 0;
+}
+
+void
+GenerateNumbers(int array[], int size)
+{
+	srand(time(NULL));
+	for (int i = 0; i < size; ++i)
+	{
+		array[i] = (1 + (rand() % 100));
+	}
+}
+
+void
+PrintArray(int array[], int size, char *text)
+{
+	printf("%s { ", text);
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("}\n");
+}
+
+void
+Swap(int *x, int *y)
+{
+	*x ^= *y;
+	*y ^= *x;
+	*x ^= *y;
+}
+
+void
+BubbleSort(int array[], int size)
+{
+	bool swapped = false;
+	for (int i = 0; i < size - 1; ++i)
+	{
+		for (int j = 0; j < size - (i + 1); ++j)
+		{
+			if (array[j] > array[j + 1])
+			{
+				Swap(&array[j], &array[j + 1]);
+				swapped = true;
+			}
+		}
+	}
+}
+
+void
+SelectionSort(int array[], int size)
+{
+	int min_index = 0;
+	for (int i = 0; i < size - 1; ++i)
+	{
+		min_index = i;
+		for (int j = i + 1; j < size; ++j)
+		{
+			if (array[min_index] > array[j])
+			{
+				min_index = j;
+			}
+		}
+		if (min_index != i)
+		{
+			Swap(&array[min_index], &array[i]);
+		}
+	}
 }
 
