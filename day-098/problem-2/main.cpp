d74b2fbb9a7acc7efd2e1ba860a267de473f2563
Nick Kane <nkanedev@gmail.com>
2017-08-25 10:46:25 -0400

    adding problem definitions for day 98

---
+++ day-098/problem-2/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Write a C function named toUpper() that converts individual lowcase letters into
+ * uppercas letters. The expression ch - 'a' + 'A' can be used to make the conversion
+ * for any lowercase character stored in ch.
+ *
+ * Add a data input check to the function written above to verify that a valid lowercase
+ * letter is passed to the function. A character is lowercase if it is greater than or equal
+ * to a and less than or equal to z. If the character is not a valid lowercase letter, have
+ * the function toUperr() return the passed character unaltered.
+ *
+ * Write a C program that accepts a string from a terminal and converts all lowercase letters
+ * in the string to uppercase leters.
+ *
+ */
+
+#include <stdio.>
+
+int
+main()
+{
+	return 0;
+}
---
+++ day-098/problem-3/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Write a C program that accepts a string from a terminal and converts
+ * all uppercase letters in the string to lowercase letters.
+ *
+ */
+
+#include <stdio.>
+
+int
+main()
+{
+	return 0;
+}
