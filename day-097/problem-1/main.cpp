fbcb18d35afaf6bbefd34bd8f785d3a3a994a197
Nick Kane <nkanedev@gmail.com>
2017-08-24 20:44:20 -0400

    completed problem 1 for day 97

--- day-097/problem-1/main.cpp
+++ day-097/problem-1/main.cpp
@@ -4,9 +4,59 @@
  *
  */
 
+#include <stdio.h>
+
+#define global_variable static
+
+global_variable const int Size = 256;
+
+void
+GetLine(char bufferString[], int size);
+
+void
+ReverseString(char bufferString[], int size);
+
 int
 main()
 {
+	char BufferString[256] = { 0 };
+
+	printf("Enter in a string:\n");
+	GetLine(BufferString, Size);
+	ReverseString(BufferString, Size);
+
+	printf("%s", BufferString);
+
 	return 0;
+}
+
+void
+GetLine(char bufferString[], int size)
+{
+	int i = 0;
+	while (i < (size - 1) && (bufferString[i++] = getchar()) != '\n');
+	bufferString[i - 1] = '\0';
+}
+
+void
+ReverseString(char bufferString[], int size)
+{
+	// find the end of the string
+	int end = 0;
+	int mid = 0;
+	while (end < (size - 1) && bufferString[end] != '\0')
+	{
+		++end;
+	}
+	mid = (end / 2);
+	end -= 1;
+	char temp;
+	for (int i = 0; i < mid; ++i)
+	{
+		temp = bufferString[i];
+		bufferString[i] = bufferString[end];
+		bufferString[end] = temp;
+		--end;
+	}
 }
 
