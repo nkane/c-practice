e6f2193707987bbeceb3bd52a377a784658a4669
Nick Kane <nkanedev@gmail.com>
2017-08-30 20:01:59 -0400

    complete problem 1 for day 100

--- day-100/problem-1/main.c
+++ day-100/problem-1/main.c
@@ -2,10 +2,35 @@
  * Enter and execute Program 9.6 on your computer.
  */
 
+#include <ctype.h>
 #include <stdio.h>
+
+#define Size 100
+
+void
+ConvetToUpper(char message[]);
 
 int
 main()
 {
+	char Message[Size] = { 0 };
+
+	printf("\nType in any sequence of characters:\n");
+	gets(Message);
+
+	ConvetToUpper(Message);
+
+	printf("The characters just entered, in uppercase are:\n%s\n", Message);
+
 	return 0;
 }
+void
+ConvetToUpper(char message[])
+{
+	int i;
+	for (i = 0; message[i] != '\0'; ++i)
+	{
+		message[i] = toupper(message[i]);
+	}
+}
+
