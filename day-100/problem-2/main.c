71114d6dd41f18f214a1b255e93a7cabc4dbfb2d
Nick Kane <nkanedev@gmail.com>
2017-08-30 20:23:43 -0400

    completed problem 2 for day 100

--- day-100/problem-2/main.c
+++ day-100/problem-2/main.c
@@ -1,12 +1,70 @@
 /*
- * Write a C function named trimfrnt() that deletes all leading blanks from a string.
+ * Write a C function named TrimFront() that deletes all leading blanks from a string.
  *
  */
 
 #include <stdio.h>
 
+#define len(array)(sizeof(array)/sizeof(array[0]))
+#define Size 100
+
+void
+GetLine(char *stringBuffer, int size);
+
+void
+TrimFront(char *stringBuffer, int size);
+
 int
 main()
 {
+	char StringBuffer[Size] = { 0 };
+
+	printf("Enter in a string:\n");
+	GetLine(StringBuffer, len(StringBuffer));
+	
+	printf("Current String:\n%s", StringBuffer);
+
+	TrimFront(StringBuffer, Size);
+
+	printf("After Front Trim:\n%s", StringBuffer);
+
 	return 0;
 }
+
+void
+GetLine(char *stringBuffer, int size)
+{
+	int i = 0;
+	while (i < (size - 1) && (*(stringBuffer + i++) = getchar()) != '\n');
+	*(stringBuffer + i) = '\0';
+}
+
+void
+TrimFront(char *stringBuffer, int size)
+{
+	int headPosition = -1;
+	int lastPosition = 0;
+	while (*(stringBuffer + lastPosition++) != '\0')
+	{
+		if (*(stringBuffer + lastPosition) != ' ' && headPosition == -1)
+		{
+			headPosition = lastPosition;
+		}
+	}
+
+	int i = 0;
+	while (*(stringBuffer + i) != '\0')
+	{
+		if (headPosition < lastPosition)
+		{
+			*(stringBuffer + i) = *(stringBuffer + headPosition++);
+		}
+		else
+		{
+			*(stringBuffer + i) = '\0';
+			break;
+		}
+		++i;
+	}
+}
+
