2b7b73455cd91a167a35565851638d2c79f4b1fd
Nick Kane <nkanedev@gmail.com>
2017-08-31 20:34:10 -0400

    complete problem 3 for day 101

--- day-101/problem-3/main.c
+++ day-101/problem-3/main.c
@@ -1,14 +1,59 @@
 /*
- * Write a function named countlets() that return the number of letters in an
- * entered string. Digits, spsaces, punctuation, tabs, and newline characters
+ * Write a function named countlets() that returns the number of letters in an
+ * entered string. Digits, spaces, punctuation, tabs, and newline characters
  * should not be included in the returned count.
  *
  */
 
 #include <stdio.h>
 
+#define Size 256
+
+void
+GetLine(char *stringBuffer, int size);
+
+int
+CountLetters(char *stringBuffer);
+
 int
 main()
 {
+	char StringBuffer[Size] = { 0 };
+
+	printf("Enter in a string:\n");
+
+	GetLine(StringBuffer, Size);
+
+	printf("String: %s\n", StringBuffer);
+	printf("Letter Count: %d\n", CountLetters(StringBuffer));
+
 	return 0;
 }
+
+void
+GetLine(char *stringBuffer, int size)
+{
+	int i = 0;
+	while (i < (size - 1) && (*(stringBuffer + i) = getchar()) != '\n')
+	{
+		++i;
+	}
+	*(stringBuffer + i) = '\0';
+}
+
+int
+CountLetters(char *stringBuffer)
+{
+	int count = 0;
+	char c; 
+	for (int i = 0; *(stringBuffer + i) != '\0'; ++i)
+	{
+		c = (*(stringBuffer + i));
+		if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		{
+			++count;
+		}
+	}
+	return count;
+}
+
