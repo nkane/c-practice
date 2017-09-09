b92ebd193ea00ce9decee14f4a9873ae8e43c53e
Nick Kane <nkanedev@gmail.com>
2017-09-08 22:54:35 -0400

    completed day 104

--- day-104/problem-2/main.c
+++ day-104/problem-2/main.c
@@ -7,11 +7,71 @@
  *
  */
 
+#include <stdbool.h>
 #include <stdio.h>
+
+#define Size 40
+
+bool
+IsValidReal64(char string[]);
 
 int
 main()
 {
+	char value[Size]= { 0 };
+
+	printf("Enter in real64 value: ");
+	gets(value);
+
+	if (IsValidReal64(value))
+	{
+		printf("Valid value: %s\n", value);
+	}
+	else
+	{
+		printf("Invalid value: %s\n", value);
+	}
+
 	return 0;
+}
+
+bool
+IsValidReal64(char string[])
+{
+	bool result = true;
+	int i = 0;
+	int d = 0;
+
+	// check for front positive or negative sign
+	if (string[i] == '-' || string[i] == '+')
+	{
+		++i;
+	}
+
+	// check for decimal
+	while (string[i] != '\0')
+	{
+		if (string[i] == '.')
+		{
+			if (d > 0)
+			{
+				result = false;
+				break;
+			}
+			++d;
+		}
+		else
+		{
+			// value doesn't fall between 0 - 9
+			if (string[i] < '0' || string[i] > '9')
+			{
+				result = false;
+				break;
+			}	
+		}
+		++i;
+	}
+
+	return result;
 }
 
