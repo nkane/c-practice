a0104cbd4439a550dc1ca961d62c0d7f8d7b6124
Nick Kane <nkanedev@gmail.com>
2017-09-13 21:34:30 -0400

    completed day 108 and move incompleted problem for day 109

--- day-108/problem-1/main.c
+++ day-108/problem-1/main.c
@@ -4,10 +4,101 @@
  *
  */
 
+#include <stdbool.h>
 #include <stdio.h>
+
+#define Size 1000
+
+typedef struct _result
+{
+	int Sentences;
+	int Words;
+} Result;
+
+void
+GetString(char *stringBuffer, int max);
+
+Result *
+Process(char *stringBuffer);
 
 int
 main()
 {
+	char StringBuffer[Size] = { 0 };
+	Result *R = NULL; 
+
+	printf("Enter in a string of text (EOF, ends):\n");
+	GetString(StringBuffer, Size);
+
+	R = Process(StringBuffer);
+
+	if (R) 
+	{
+		printf("Sentences: %d\n", R->Sentences);
+		printf("Words: %d\n", R->Words);
+		free(R);
+	}
+	else
+	{
+		printf("Ouch! something went wrong!\n");
+	}
+
 	return 0;
 }
+
+void
+GetString(char *stringBuffer, int max)
+{
+	int i = 0;
+	while (i < (max - 1) && (*(stringBuffer + i) = getchar()) != EOF)
+	{
+		++i;
+	}
+	*(stringBuffer + i) = '\0';
+}
+
+
+Result *
+Process(char *stringBuffer)
+{
+	Result *r = (Result *)malloc(sizeof(Result));
+	r->Sentences = 0;
+	r->Words = 0;
+	int i = 0;
+	char c;
+	bool inWord = false;
+	bool lastCharPunc = false;
+	while (*(stringBuffer + i) != '\0')
+	{
+		c = *(stringBuffer + i);
+		switch (c)
+		{
+			case '.':
+			case '?':
+			case '!':
+			{
+				if (!lastCharPunc)
+				{
+					++r->Sentences;
+				}
+				lastCharPunc = true;
+			} break;
+			case ' ':
+			{
+				inWord = false;
+			} break;
+			default: 
+			{
+				lastCharPunc = false;
+				if (inWord == false)
+				{
+					inWord = true;
+					++r->Words;
+				}
+			} break;
+		}
+		++i;
+	}
+	return r;
+}
+
