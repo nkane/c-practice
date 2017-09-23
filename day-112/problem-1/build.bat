74f632ca2836819e72eb0ea826c187576930a5f4
Nick Kane <nkanedev@gmail.com>
2017-09-22 20:41:54 -0400

    completed problem 1 for day 112

--- day-112/problem-1/build.bat
+++ day-112/problem-1/build.bat
@@ -3,4 +3,6 @@ pushd .\build
 
 cl -Od -MTd -Zi -nologo ..\main.c
 
+xcopy /Y /S /I ..\data .\data
+
 popd
--- day-112/problem-1/main.c
+++ day-112/problem-1/main.c
@@ -13,8 +13,48 @@
 
 #include <stdio.h>
 
+void
+OpenFile(char *file, char *options, FILE *outFile);
+
 int
 main()
 {
+	FILE *memoFile = NULL;
+	FILE *couponsFile = NULL;
+	FILE *pricesFile = NULL;
+
+	OpenFile("./data/coba.mem", "rb", memoFile);
+	OpenFile("./data/coupons.bnd", "ab", couponsFile);
+	OpenFile("./data/prices.dat", "wb", pricesFile);
+
+	if (memoFile)
+	{
+		fclose(memoFile);
+	}
+
+	if (couponsFile)
+	{
+		fclose(couponsFile);
+	}
+
+	if (pricesFile)
+	{
+		fclose(pricesFile);
+	}
+
 	return 0;
 }
+
+void
+OpenFile(char *file, char *options, FILE *outFile)
+{
+	if (outFile = fopen(file, options))
+	{
+		printf("SUCCESS - Properly opened %s\n", file);
+	}
+	else
+	{
+		printf("ERROR - Failed to open file %s\n", file);
+	}
+}
+
