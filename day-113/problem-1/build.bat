41b649d5fdccf66a331da79b3b95b306cd21bb35
Nick Kane <nkanedev@gmail.com>
2017-09-23 10:48:08 -0400

    completed day-113

--- day-113/problem-1/build.bat
+++ day-113/problem-1/build.bat
@@ -3,4 +3,6 @@ pushd .\build
 
 cl -Od -MTd -Zi -nologo ..\main.c
 
+xcopy /Y /Q /I ..\data .\data
+
 popd
--- day-113/problem-1/main.c
+++ day-113/problem-1/main.c
@@ -6,10 +6,87 @@
  *
  */
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define len(array)(sizeof(array)/sizeof(array[0]))
+
+void
+ReadFile(char *name);
+
+void
+ClearStringBuffer(char *stringBuffer);
+
+bool
+IsTerminatingCharacter(char c);
 
 int
-main()
+main(int argc, char *argv[])
 {
+	if (argc > 1)
+	{
+		ReadFile(argv[1]);
+	}
+
 	return 0;
 }
+
+void
+ReadFile(char *fileName)
+{
+	FILE *readFile = NULL;
+	char stringBuffer[256] = { 0 };
+	char readContinue = '\0';
+	if (readFile = fopen(fileName, "r"))
+	{
+		do
+		{
+			ClearStringBuffer(stringBuffer);
+			fgets(stringBuffer, 256, readFile);
+			if (IsTerminatingCharacter(*(stringBuffer + 0)))
+			{
+				break;
+			}
+			puts(stringBuffer);
+			printf("continue (y/n) ...\n");
+			scanf("\n%c", &readContinue);
+			readContinue = tolower(readContinue);
+		} while (readContinue == 'y');
+		fclose(readFile);
+	}
+	else
+	{
+		printf("ERROR! Failed to open file: %s\n", fileName);
+	}
+}
+
+void
+ClearStringBuffer(char *stringBuffer)
+{
+	for (int i = 0; i < len(stringBuffer); ++i)
+	{
+		*(stringBuffer + i) = '\0';
+	}
+}
+
+bool
+IsTerminatingCharacter(char c)
+{
+	bool result = false;
+	switch (c)
+	{
+		case '\0':
+		case EOF:
+		{
+			result = true;
+		} break;
+
+		default:
+		{
+
+		} break;
+	}
+	return result;
+}
+
