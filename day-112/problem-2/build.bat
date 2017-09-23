69ed1c582a4ca050d5e2f859c19bd2111c2cfb2a
Nick Kane <nkanedev@gmail.com>
2017-09-22 21:30:35 -0400

    completed problem 2 for day 112

--- day-112/problem-2/build.bat
+++ day-112/problem-2/build.bat
@@ -1,6 +1,8 @@
 IF NOT EXIST .\build mkdir .\build
 pushd .\build
 
+IF NOT EXIST .\data mkdir .\data
+
 cl -Od -MTd -Zi -nologo ..\main.c
 
 popd
--- day-112/problem-2/main.c
+++ day-112/problem-2/main.c
@@ -4,16 +4,76 @@
  * and empty line is entered. An empty line is a line with no text - just a new
  * line caused by pressing the Enter key.
  *
- * Replace the gets() function in the program fwith an equivalent call to fgets().
+ * Replace the gets() function in the program with an equivalent call to fgets().
  *
  * Modify the program to read and display the data stored in the text.dat file.
  *
  */
 
+#include <stdbool.h>
 #include <stdio.h>
+
+#define Size 1024
+
+void
+WriteTextToFile(char *stringBuffer, char *outputFileName);
+
+bool
+IsTerminatedString(char c);
 
 int
 main()
 {
+	char StringBuffer[Size] = { 0 };
+
+	int i = 0;
+	printf("*** Output will be written to ./data/text.dat ***\n");
+	printf("Enter in lines of text here:\n");
+
+	WriteTextToFile(StringBuffer, "./data/text.dat");
+
 	return 0;
 }
+
+void
+WriteTextToFile(char *stringBuffer, char *outputFileName)
+{
+	FILE *outputFile = fopen(outputFileName, "a+");
+	if (outputFile)
+	{
+		do 
+		{
+			/*
+			gets(stringBuffer);
+			fputs(stringBuffer, outputFile);
+			*/
+			fgets(stringBuffer, Size, stdin);
+			if (!IsTerminatedString(*(stringBuffer + 0)))
+			{
+				fputs(stringBuffer, outputFile);
+			}
+		} while (!IsTerminatedString(*(stringBuffer + 0)));
+		fclose(outputFile);
+	}
+}
+
+bool
+IsTerminatedString(char c)
+{
+	bool result = false;
+	switch (c)
+	{
+		case ' ': 
+		case '\n':
+		case '\0':
+		{
+			result = true;
+		} break;
+
+		default:
+		{
+		} break;
+	}
+	return result;
+}
+
