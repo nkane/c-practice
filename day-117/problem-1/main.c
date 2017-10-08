052da1636dc8704d9287180174a8f507803b0668
Nick Kane <nkanedev@gmail.com>
2017-10-08 15:05:25 -0400

    adding day 117 problem with definition

---
+++ day-117/problem-1/main.c
@@ -0,0 +1,27 @@
+/*
+ * Create a file containing the following names, social security numbers,
+ * hourly rate, and hours worked:
+ *
+ *	+----------------+---------------+--------+----+
+ *	|  B. Clandwell  |  XXX-XX-XXXX  |  7.32  | 37 |
+ *	+----------------+---------------+--------+----+
+ *	|  D. Memcheck   |  XXX-XX-XXXX  |  8.32  | 40 |
+ *	+----------------+---------------+--------+----+
+ *	|  R. Potter     |  XXX-XX-XXXX  |  6.54  | 40 |
+ *	+----------------+---------------+--------+----+
+ *	|  W. Rosen      |  XXX-XX-XXXX  |  9.80  | 35 |
+ *	+----------------+---------------+--------+----+
+ * 
+ * Write a C program that reads the data file created above, and computes
+ * and displays a payroll schedule. The output should list the social security
+ * number, name, and gross pay for each individual.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
