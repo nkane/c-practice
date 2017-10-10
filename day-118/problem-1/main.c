0c37f190a300172ed8354903001debd4cff539b1
Nick Kane <nkanedev@gmail.com>
2017-10-09 22:30:18 -0400

    adding problem for day 118

---
+++ day-118/problem-1/main.c
@@ -0,0 +1,25 @@
+/*
+ * Create a file containg the following car number, number of miles drive,
+ * and number of gallons of gas used by each car:
+ *
+ *	+----+--------+------+
+ *	| 54 |   250  |  19  |
+ *	+----+--------+------+
+ *	| 62 |   525  |  38  |
+ *	+----+--------+------+
+ *	| 71 |   123  |   6  |
+ *	+----+--------+------+
+ *	| 85 |  1322  |  86  |
+ *	+----+--------+------+
+ *	| 97 |   235  |  14  |
+ *	+----+--------+------+
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
