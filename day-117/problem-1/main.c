14aba29aeb579c3e3ea2246247d0269fbeee7c48
Nick Kane <nkanedev@gmail.com>
2017-10-09 21:10:32 -0400

    completed day 117

--- day-117/problem-1/main.c
+++ day-117/problem-1/main.c
@@ -19,9 +19,42 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+
+typedef struct _record
+{
+	char Name[25];
+	char SocialNumber[16];
+	float HourlyRate;
+	int HoursWorked;
+	float GrossPay;
+} Record;
 
 int
 main()
 {
+	FILE *dataFile = NULL;
+	dataFile = fopen("./data/test.dat", "r");;
+
+	if (dataFile)
+	{
+		Record r = { 0 };
+		while (fscanf(dataFile, "%[^,],%[^,],%f,%d ", r.Name, r.SocialNumber, &r.HourlyRate, &r.HoursWorked) != EOF)
+		{
+			r.GrossPay = ((float)r.HoursWorked * r.HourlyRate);
+			printf("Name: %s\n", r.Name);
+			printf("Social Number: %s\n", r.SocialNumber);
+			printf("Hourly Rate: %6.2f\n", r.HourlyRate);
+			printf("Hours Worked: %d\n", r.HoursWorked);
+			printf("Gross Pay: %6.2f\n\n", r.GrossPay);
+			memset(r.Name, 0, 25);
+			memset(r.SocialNumber, 0, 16);
+			r.HoursWorked = 0;
+			r.HourlyRate = 0.0f;
+			r.GrossPay = 0.0f;
+		}
+		fclose(dataFile);
+	}
+
 	return 0;
 }
