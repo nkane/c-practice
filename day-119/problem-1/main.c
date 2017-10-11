09fc7add613eeb60c77fef1e2bc3b2f01404c942
Nick Kane <nkanedev@gmail.com>
2017-10-11 06:05:17 -0400

    adding day 119 problem set

---
+++ day-119/problem-1/main.c
@@ -0,0 +1,24 @@
+/*
+ * Create a file with the following data containing the part number,
+ * opening balance, number of items sold, and minimum stock required:
+ *
+ *
+ * 	Part Number    Initial Amount    Quantity Sold    Minimum Amount
+ * 	QA310          95                47               50
+ * 	CM145          320               162              200
+ * 	MS514          34                20               25
+ * 	EN212          163               150              160
+ *
+ * Write a C program to create an inventory report based on the data in the
+ * file created. The display should consist of the part number, current balance,
+ * and amount that is necessary to bring the inventory to the minimum level.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
---
+++ day-119/problem-2/main.c
@@ -0,0 +1,27 @@
+/*
+ * Create a file containing the following data:
+ *
+ * Name    		Rate    	Hours
+ * Callaway, G		6.00		40
+ * Hanson, P.		5.00		48
+ * Lasard, D.		6.50		35
+ * Stillman, W.		8.00		50
+ *
+ * Write a C program that uses the information contained
+ * in the file created to produce the follow pay report
+ * for each employee:
+ *
+ * Name        Rate        Hours        Regular Pay        Overtime Pay        Gross Pay
+ *
+ * Any hours worked above 40 hours are paid at time and a half; additionally, the program
+ * should display the total of the regular, overtime, and gross pay columns.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
