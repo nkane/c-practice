b2ef47ddab82597b50cda37ab5a3fc8e39913303
Nick Kane <nkanedev@gmail.com>
2017-10-11 21:22:20 -0400

    completed day 119

--- day-119/problem-1/main.c
+++ day-119/problem-1/main.c
@@ -17,8 +17,37 @@
 
 #include <stdio.h>
 
+typedef struct _record
+{
+	char PartNumber[6];
+	int InitialAmount;
+	int QuantitySold;
+	int MinimumAmount;
+} Record;
+
 int
 main()
 {
+	FILE *readFile = NULL;
+
+	if (readFile = fopen("./data/test.dat", "r"))
+	{
+		Record r = { 0 };
+		int currentBalance = 0;
+		int restockAmount = 0;
+		while (fscanf(readFile, "%[^,],%d,%d,%d ", r.PartNumber, &r.InitialAmount, &r.QuantitySold, &r.MinimumAmount) != EOF)
+		{
+			currentBalance = (r.InitialAmount - r.QuantitySold);
+			restockAmount = (r.MinimumAmount - currentBalance);
+			printf("Part Number: %s\n", r.PartNumber);
+			printf("Initial Amount: %d\n", r.InitialAmount);
+			printf("Quantity Sold: %d\n", r.QuantitySold);
+			printf("Current Balance: %d\n", currentBalance);
+			printf("Minimum Amount: %d\n", r.MinimumAmount);
+			printf("Restock Amount: %d\n\n", restockAmount);
+		}
+		fclose(readFile);
+	}
+
 	return 0;
 }
--- day-119/problem-2/main.c
+++ day-119/problem-2/main.c
@@ -2,7 +2,7 @@
  * Create a file containing the following data:
  *
  * Name    		Rate    	Hours
- * Callaway, G		6.00		40
+ * Callaway, G.		6.00		40
  * Hanson, P.		5.00		48
  * Lasard, D.		6.50		35
  * Stillman, W.		8.00		50
@@ -20,8 +20,51 @@
 
 #include <stdio.h>
 
+typedef struct _record
+{
+	char Name[20];
+	float Rate;
+	int Hours;
+} Record;
+
 int
 main()
 {
+	FILE *readFile = NULL;
+
+	if (readFile = fopen("./data/test.dat", "r"))
+	{
+		int overtimeHours = 0;
+		float regularPay = 0.0f;
+		float overtimePay = 0.0f;
+		Record r = { 0 };
+		while (fscanf(readFile, "%[^,],%f,%d ", r.Name, &r.Rate, &r.Hours) != EOF)
+		{
+			overtimeHours = 0;
+			printf("Employee Name: %s\n", r.Name);	
+			printf("Rate: %6.2f\n", r.Rate);
+	
+			if (r.Hours > 40)
+			{
+				overtimeHours = (r.Hours - 40);
+				r.Hours -= overtimeHours;
+			}
+
+			printf("Regular Hours: %d\n", r.Hours);
+			regularPay = (float)r.Hours * r.Rate;
+			printf("Regular Pay: %6.2f\n", regularPay);
+
+			if (overtimeHours > 0)
+			{
+				printf("Overtime Hours: %d\n", overtimeHours);
+				overtimePay = ((float)overtimeHours * (r.Rate *= 1.5f));
+				printf("Overtime Pay: %6.2f\n", overtimePay);
+				printf("Total Pay: %6.2f\n", regularPay + overtimePay);
+			}
+
+			printf("\n");
+		}
+	}
+
 	return 0;
 }
