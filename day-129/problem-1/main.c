9b3f4a4ac9da4c9acd68bb1c93053dae4555ab6c
Nick Kane <nkanedev@gmail.com>
2017-11-07 21:20:13 -0500

    added problems set for day 129

---
+++ day-129/problem-1/main.c
@@ -0,0 +1,24 @@
+/*
+ * Write, compile, and execute a C program that creates a binary file named
+ * points and writes the following numbers to the file:
+ *
+ * 	6.3    8.2    18.25    24.32
+ *	4.0    4.0    10.0     -5.0
+ *     -2.0    5.0     4.0      5.0
+ *
+ * Using the data in the points file created - write, compile, and run a C
+ * program that reads four numbers using a for loop and interprets the first
+ * and second numbers in each record as the coordinates of one point and the
+ * third and fourth numbers as the coordinates of a second point. Have the
+ * program compute and display the slope and midpoint of each pair of entered
+ * points.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
---
+++ day-129/problem-2/main.c
@@ -0,0 +1,23 @@
+/*
+ * Write, compile, and run a C program that creates a binary file name grades.bin 
+ * and writes the following five lines of data to the file:
+ *
+ * 	90.3    92.7    90.3    99.8
+ * 	85.3    90.5    87.3    90.8
+ * 	93.2    88.4    93.8    75.6
+ * 	82.4    95.6    78.2    90.0
+ * 	93.5    80.2    92.9    94.4
+ *
+ * Using the data in the grades.bin file created - write, compile, and run a C
+ * program that reads, computes, and displays the average of each group of four
+ * grades.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+	return 0;
+}
