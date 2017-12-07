04f5e0cfca8bac65ab2ce890c9be90ec09d3f9d8
Nick Kane <nkanedev@gmail.com>
2017-12-06 21:35:21 -0500

    adding problem set 135

---
+++ day-135/problem-2/main.c
@@ -0,0 +1,17 @@
+/*
+ * Write a C program that stores the following numbers in an array named rates:
+ * 6.25, 6.50, 6.8, 7.2, 7.35, 7.5, 7.65, 7.8, 9.2, 9.4, 9.6, 9.8, 9.0. Display the
+ * values in thet array by changing the address in a pointer called dispPt. Use a
+ * for statement in your program.
+ *
+ * Modify the program written to use a while statement.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
