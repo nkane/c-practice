9efdf143df51bd4e669d20494d13e9a983f5e4e2
Nick Kane <nkanedev@gmail.com>
2017-12-18 08:49:26 -0500

    completed day 142 - set up day 143

--- day-142/problem-1/main.c
+++ day-142/problem-1/main.c
@@ -18,8 +18,47 @@
 
 #include <stdio.h>
 
+#define len(array)(sizeof(array)/sizeof(array[0]))
+
+typedef struct _monthDays
+{
+    char name[10];
+    int days;
+} MonthDays;
+
+void
+DisplayMonthDays(MonthDays *md, int size);
+
 int
 main()
 {
+    MonthDays convert[12] = 
+    {
+        { "January", 31 },
+        { "February", 28 },
+        { "March", 31 },
+        { "April", 30 },
+        { "May", 31 },
+        { "June", 30 },
+        { "July", 31 },
+        { "August", 31 },
+        { "September", 30 },
+        { "October", 31 },
+        { "November", 30 },
+        { "December", 31 },
+    };
+
+    DisplayMonthDays(convert, len(convert));
+
     return 0;
+}
+
+void
+DisplayMonthDays(MonthDays *mdArray, int size)
+{
+    int i;
+    for (i = 0; i < size; ++i)
+    {
+        printf("%s : days: %d\n", (char *)(mdArray + i)->name, (int)(mdArray + i)->days);
+    }
 }
--- day-142/problem-2/main.c
+++ day-142/problem-2/main.c
@@ -8,8 +8,39 @@
 
 #include <stdio.h>
 
+typedef struct _monthDays
+{
+    char name[10];
+    int days;
+} MonthDays;
+
 int
 main()
 {
+    int input = 0;
+    MonthDays md[12] = 
+    {
+        { "January", 31 },
+        { "February", 28 },
+        { "March", 31 },
+        { "April", 30 },
+        { "May", 31 },
+        { "June", 30 },
+        { "July", 31 },
+        { "August", 31 },
+        { "September", 30 },
+        { "October", 31 },
+        { "November", 30 },
+        { "December", 31 },
+    };
+
+    do {
+        printf("Enter in the month number: ");
+        scanf("%d", &input);
+    } while (input <= 0 || input > 12);
+    --input;
+
+    printf("Month: %s - days: %d\n", (md + input)->name, (md + input)->days);
+
     return 0;
 }
