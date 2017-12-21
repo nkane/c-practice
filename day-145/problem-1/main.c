840e3a5cacdd4fe28281b9d19f76e420539ee416
Nick Kane <nkanedev@gmail.com>
2017-12-21 07:58:32 -0500

    adding problem set for day 145

---
+++ day-145/problem-1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Construct a set of parrallel arrays consisting of 10 integer indentifcation numbers and 10
+ * pay rates. Then, determine the identification number that correspond to the highest pay rate.
+ * Make sure to locate the highest pay rate by sorting the pay rate array so that the hight pay
+ * rate has a subscript value of 0.
+ *
+ * What problems do you encounter in determining the correct identification number
+ * when you sort the pay rate array? 
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
