01aac6d1fac0644200ea56a4db8a70c24724c967
Nick Kane <nkanedev@gmail.com>
2017-12-28 08:48:12 -0500

    complete day 146

--- day-146/problem-1/main.c
+++ day-146/problem-1/main.c
@@ -17,8 +17,78 @@
 
 #include <stdio.h>
 
+typedef struct _date
+{
+    int month;
+    int day;
+    int year;
+} Date;
+
+Date
+RequestDateInput();
+
+int
+DayDiffFromDates(Date beginDate, Date diffDate);
+
 int
 main()
 {
+    Date defaultDate = (Date)
+        {
+            .month = 1,
+            .day = 1, 
+            .year = 1900
+        };
+
+    Date inputDate = RequestDateInput();
+
+    printf("Day difference: %d\n", DayDiffFromDates(defaultDate, inputDate));
+
+    system("pause");
+
     return 0;
+}
+
+Date
+RequestDateInput()
+{
+    Date result = { 0 };
+
+    do {
+        printf("Input a month: ");
+        scanf("%d", &result.month);
+    } while (result.month > 12 || result.month < 1);
+
+    do {
+        printf("Input a day: ");
+        scanf("%d", &result.day);
+    } while (result.day > 30 || result.day < 1);
+
+    do {
+        printf("Input a year: ");
+        scanf("%d", &result.year);
+    } while (result.year < 1900);
+
+    return result;
+}
+
+int
+DayDiffFromDates(Date beginDate, Date diffDate)
+{
+    int result = 0;
+    int currentDiff = 0;
+
+    // NOTE(nick): diff between years - default day for a year is 360 days
+    currentDiff = (diffDate.year - beginDate.year);
+    result += (currentDiff * 360);
+
+    // NOTE(nick): diff between months - default days for a month is 30 days
+    currentDiff = (diffDate.month - beginDate.month);
+    result += (currentDiff * 30);
+
+    // NOTE(nick: diff between days
+    currentDiff = (diffDate.day - beginDate.day);
+    result += currentDiff;
+
+    return result;
 }
