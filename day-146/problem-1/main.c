b908e0e30d2cf286132df5e106d04fa1fb55a6e7
Nick Kane <nkanedev@gmail.com>
2017-12-27 20:33:48 -0500

    adding problem for day 146

---
+++ day-146/problem-1/main.c
@@ -0,0 +1,24 @@
+/*
+ * Write a C function named days() that determines the number of days from the date
+ * 1/1/1900 for any date passed as a structure. Use the following Date structure:
+ *
+ * struct Date
+ * {
+ *      int month;
+ *      int day;
+ *      int year;
+ * };
+ *
+ * In writing the days() function, use the convention that all years have 360 days and
+ * each month consist of 30 days. The function should return the number of days for any
+ * date structure passed to it.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
