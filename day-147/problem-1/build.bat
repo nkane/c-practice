27089727e121af3044b92f70734be7fc73e5d42f
Nick Kane <nkanedev@gmail.com>
2017-12-29 22:14:18 -0500

    adding problem for day 147

---
+++ day-147/problem-1/build.bat
@@ -0,0 +1,6 @@
+IF NOT EXIST .\build MKDIR .\build
+PUSHD .\build
+
+cl /Od /Zi /MTd /nologo ..\main.c
+
+POPD
---
+++ day-147/problem-1/main.c
@@ -0,0 +1,23 @@
+/*
+ * Write a C function named diffDays() that calculates and returns the difference between two days.
+ * Each date is passed to the function as a struct using the following global structure:
+ *
+ *  struct Date
+ *  {
+ *      int month;
+ *      int day;
+ *      int year;
+ *  };
+ *
+ *  The diffDays() function should make two calls to the days() function written for the previous
+ *  day's exercise.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
