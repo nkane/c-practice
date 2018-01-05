28e4db043cf8779e57628ed6827a795de5d22300
Nick Kane <nkanedev@gmail.com>
2018-01-05 07:05:57 -0500

    adding problem set for day 154

---
+++ day-154/problem-1/main.c
@@ -0,0 +1,13 @@
+/*
+ * Write a stack program that accepts a struture consisting of interger identification
+ * numbers and a floating-point hourly pay rate.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
---
+++ day-154/problem-2/main.c
@@ -0,0 +1,14 @@
+/*
+ * Add a menu function to Program 13.5 that gives the user a choice of adding
+ * a name to the stack, removing a name from the stack, or listing
+ * the contents of the stack without removing any structures from it.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
