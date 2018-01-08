598b6fb1cf322b0d3fafdcbbe7cd94fcb5bda035
Nick Kane <nkanedev@gmail.com>
2018-01-08 06:52:08 -0500

    adding day 156 problem

---
+++ day-156/problem-1/main.c
@@ -0,0 +1,13 @@
+/*
+ * Modify program 13.6 so that the arguments to both enque and dequeue
+ * are a structure name rather than a single field varible.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
