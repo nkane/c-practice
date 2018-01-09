902148c4d23809e101b5294d2219dbc7cf034f5e
Nick Kane <nkanedev@gmail.com>
2018-01-09 06:34:42 -0500

    adding problem for day 157

---
+++ day-157/problem-1/main.c
@@ -0,0 +1,13 @@
+/*
+ * Write a queue program that accepts a structure consisting of an integer
+ * identification number and a floating-point hourly pay rate.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
