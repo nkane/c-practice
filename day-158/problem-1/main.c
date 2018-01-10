b9b7c97d6f1fe71eb5c2862ad712a3dcddb27eac
Nick Kane <nkanedev@gmail.com>
2018-01-10 07:11:48 -0500

    adding problem for day 158

---
+++ day-158/problem-1/main.c
@@ -0,0 +1,14 @@
+/*
+ * Add a menu function to Program 13.6 that gives the user a choice of adding a name to
+ * the queue, remving a name from the queue, or listing the contents of the queue without
+ * removing any structures from it.
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
