98513b0b0b64945b11de4ebc1d95f29e372a4bf3
Nick Kane <nkanedev@gmail.com>
2018-01-12 06:26:18 -0500

    adding problem for day 159

---
+++ day-159/problem-1/main.c
@@ -0,0 +1,13 @@
+/*
+ * Write a program for deleting an existing structure from a linked list of structures 
+ * created by program 13.7
+ *
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    return 0;
+}
